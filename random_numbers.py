344dd121d5ed400cd0a9d9def6bcf7481aa40b5c
LyraDuyu <lyraduyu@gmail.com>
2024-05-12 17:13:07 +0200

    finish task

---
+++ random_numbers.py
@@ -0,0 +1,13 @@
+# generate 5 random numbers between 0 and 15
+import random
+random.randint(10,15)
+a=random.randint(10,15)
+print(a)
+for i in range(5):
+    print(random.randint(10,15)
+        
+random_numbers=[]
+for i in range(5):
+    random_numbers.append(random.randint(10,15))
+
+print(random_numbers)
